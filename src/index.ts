61471beb6b0934f3cc95c7f05ac73118fd682133
sbmsr <messs@seas.upenn.edu>
2022-06-03 23:28:09 -0400

    get cookies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import { getCookies } from "https://deno.land/std/http/mod.ts";
 
 const port = parseInt(Deno.env.get("PORT") ?? "8000");
-serve(() => new Response("Choo Choo! Welcome to your Deno app\n"),
-      { addr: `:${port}` });
 
-console.log(`http://localhost:${port}/`);
+const handler = (request: Request): Response => {
+  return new Response(JSON.stringify(getCookies(request.headers)), { status: 200 });
+};
+
+console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
+await serve(handler, { port });
