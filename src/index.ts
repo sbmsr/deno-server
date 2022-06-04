992525d874e3e7f1c7831c92f8512facd5014228
sbmsr <messs@seas.upenn.edu>
2022-06-03 23:51:47 -0400

    deno 1.22.2

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
-// import { getCookies } from "https://deno.land/std/http/mod.ts";
-// import { Application } from "https://deno.land/x/oak/mod.ts";
-
-// const app = new Application();
-
-// app.use(({ request, response }) => {
-//   response.body = JSON.stringify(getCookies(request.headers));
-// });
-
-// const port = parseInt(Deno.env.get("PORT") ?? "8000");
-// await app.listen({ port: port });
+import { getCookies } from "https://deno.land/std/http/mod.ts";
 import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+app.use(({ request, response }) => {
+  response.body = JSON.stringify(getCookies(request.headers));
 });
 
-await app.listen({ port: 8000 });
+const port = parseInt(Deno.env.get("PORT") ?? "8000");
+await app.listen({ port: port });
